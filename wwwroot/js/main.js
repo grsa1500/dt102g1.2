6801bb7c70f83445603105c1b9482d36af5bba6c
grsa1500 <grsa1500@student.miun.se>
2020-01-28 17:37:30 +0100

    Add project files.

---
+++ wwwroot/js/main.js
@@ -0,0 +1,26 @@
+﻿"use strict"
+
+//Läser in variabler
+var menu = document.getElementById('menu-btn');
+var nav = document.getElementById('nav');
+var open = false;
+
+//Eventlistener vid klick på meny
+menu.addEventListener('click', openMenu);
+
+
+//Kollar om menyn är öppen. Om inte öppen, öppnas den. Om öppen, stängs den.
+function openMenu() {
+
+	if (!open) {
+		nav.style.display = "block";
+		menu.innerHTML = "Stäng";
+		open = true;
+	}
+
+	else {
+		nav.style.display = "none";
+		menu.innerHTML = "Meny";
+		open = false;
+	}
+}
